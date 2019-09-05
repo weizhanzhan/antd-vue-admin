f7b1e20406ef08c88784a81d02c7da204b9c6189
weizhanzhan <375786117@qq.com>
2019-09-05 18:40:54 +0800

    加入ts规范

--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,20 @@ module.exports = {
         `
       }
     }
+  },
+  configureWebpack: {
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+    module: {
+      rules: [{
+        test: /.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          appendTsSuffixTo: [/.vue$/]
+        }
+      }]
+    }
   }
 }
