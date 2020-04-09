8aee55f286253c6af75a749ba21afa782b73b04f
weizhanzhan <375786117@qq.com>
2020-04-09 17:53:44 +0800

    config.js

--- dist/js/chunk-76eee2f3.58b77f41.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-76eee2f3"],{a227:function(n,w,e){}}]);
-//# sourceMappingURL=chunk-76eee2f3.58b77f41.js.map
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    productionSourceMap: process.env.NODE_ENV !== 'production'
+}
