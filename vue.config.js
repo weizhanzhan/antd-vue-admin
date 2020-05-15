645736025f6bb02c7e762b82641cc6f82395a671
weizhanzhan <375786117@qq.com>
2020-05-15 16:15:57 +0800

    功能开发细节优化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    publicPath: '/vue-admin-preview/',
     productionSourceMap: process.env.NODE_ENV !== 'production'
     // baseUrl: process.env.NODE_ENV === 'production' ? './' : ''
 }
