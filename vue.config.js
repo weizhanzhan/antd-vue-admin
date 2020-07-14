d4ca77582e9c423c6e488b69884cafe481a4d3c4
weizhanzhan <375786117@qq.com>
2020-07-14 17:33:06 +0800

    功能开发细节优化

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
     publicPath: '/vue-admin-preview/',
-    productionSourceMap: process.env.NODE_ENV !== 'production'
+    productionSourceMap: process.env.NODE_ENV !== 'production',
+    configureWebpack: config => {
+        config.externals = {
+          vue: 'Vue',
+          'vue-router': 'VueRouter',
+          vuex: 'Vuex',
+          axios: 'axios'
+        }
+      }
     // baseUrl: process.env.NODE_ENV === 'production' ? './' : ''
 }
